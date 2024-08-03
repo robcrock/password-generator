78c5d8eb492eabfebb7406ec9f86ea31b3a01aff
Robert Crocker <rcrocker13@gmail.com>
2024-08-03 17:01:20 -0500

    Remove boiler plate and add starter code from Frontend Mentor.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Password Generator P4$5W0rD! Character Length Include Uppercase Letters
+      Include Lowercase Letters Include Numbers Include Symbols Strength
+      Generate
+    </main>
   );
 }
